0309f51098d116cc64f5bf46677a8f57f4a5d382
mdhasibulislam <hhasibulislam503@gmail.com>
2020-05-09 15:26:29 +0600

    active box project

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(function)(){
+  
+    
+    
+    
+    
+    //slider//////////
+    
+     $('.slider-text').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: false,        
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+    
+  
+    
+    );
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+};
